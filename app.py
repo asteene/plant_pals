787fd8cb05e86d1d57653812496bec69a1f39ddc
parth448812 <parthkpatel04@gmail.com>
2024-09-19 17:23:04 -0400

    foundation setup

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flaskr import createApp
+from flask import Flask, render_template
 
-app = createApp()
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, world!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
