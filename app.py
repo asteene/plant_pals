4f1b22d17bf440dfdccbda57f45a4d5173dc6bd7
asteene <asteene10@gmail.com>
2024-10-02 10:32:27 -0400

    fixed app.py file

--- app.py
+++ app.py
@@ -1,45 +1,6 @@
 from flaskr import createApp
-from flask import Flask, render_template
 
-app = Flask(__name__)
-
-#Nav bar
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/new")
-def new_post():
-    return render_template("new.html")
-
-@app.route("/journals")
-def journals():
-    return render_template("journals.html")
-
-@app.route("/profile") #Once the account feature is created the router will include idenitfying profile information. I'm also assuming the profile page can act as the users & friends profiles
-def profile():
-    return render_template("profile.html")
-
-@app.route("/settings")
-def settings():
-    return render_template("settings.html")
-
-#Post pages 
-
-@app.route("/journal") #Once journals DB is made routing will include journal information 
-def journal():
-    return render_template("journal.html")
-
-@app.route("/journal/entry") #Once journals DB is made routing will include journal & entry information 
-def entry():
-    return render_template("entry.html")
-
-#HTML file to test out functions. Delete later
-@app.route("/example")
-def example_file():
-    return render_template("example.html")
-
-
+app = createApp()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flaskr/main/routes.py
+++ flaskr/main/routes.py
@@ -1,8 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-@main.route('/home')
+@main.route("/")
 def home():
-    return "Hello, world!"
+    return render_template("index.html")
+
+@main.route("/new")
+def new_post():
+    return render_template("new.html")
+
+@main.route("/journals")
+def journals():
+    return render_template("journals.html")
+
+@main.route("/profile") #Once the account feature is created the router will include idenitfying profile information. I'm also assuming the profile page can act as the users & friends profiles
+def profile():
+    return render_template("profile.html")
+
+@main.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+#Post pages 
+
+@main.route("/journal") #Once journals DB is made routing will include journal information 
+def journal():
+    return render_template("journal.html")
+
+@main.route("/journal/entry") #Once journals DB is made routing will include journal & entry information 
+def entry():
+    return render_template("entry.html")
+
+#HTML file to test out functions. Delete later
+@main.route("/example")
+def example_file():
+    return render_template("example.html")
