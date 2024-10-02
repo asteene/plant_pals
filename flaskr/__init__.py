d55c58520f196624ea11a7950c1a7d174ac9128b
asteene <asteene10@gmail.com>
2024-10-02 10:32:27 -0400

    fixed app.py file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,10 +5,10 @@ from flask_bcrypt import Bcrypt
 
 from flaskr.config import Config
 
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = 'main.login'
-bcrypt = Bcrypt()
+#db = SQLAlchemy()
+#login_manager = LoginManager()
+#login_manager.login_view = 'main.login'
+#bcrypt = Bcrypt()
 
 def createApp(config_class=Config):
     '''  
@@ -18,9 +18,9 @@ def createApp(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)
-    login_manager.init_app(app)
-    bcrypt.init_app(app)
+    #db.init_app(app)
+    #login_manager.init_app(app)
+    #bcrypt.init_app(app)
 
     from flaskr.main.routes import main
 
